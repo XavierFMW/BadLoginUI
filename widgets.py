e15fcc02ae7ed87b2f6cbe479f04444cb7a2c6fa
Xavier M.W <69274109+XavierFMW@users.noreply.github.com>
2022-09-19 17:34:37 -0600

    Added Dropdown Widget
    Added the dropdown entry widgets, fixed framing issues.

---
+++ widgets.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+
+
+class DropdownTextEntry:
+
+	CHARSET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$"
+
+	def __init__(self, parent, length):
+		self.frame = self.__initialize_frame(parent)
+		self.__initialize_dropdowns(length)
+	
+	
+	def __initialize_frame(self, parent):
+		
+		frame = tk.Frame(parent)
+		return frame
+
+
+	def __initialize_dropdowns(self, length):
+		
+		for n in range(length):	
+			str_var = tk.StringVar()
+			str_var.set(" ")
+			dropdown = tk.OptionMenu(self.frame, str_var, *self.CHARSET)
+			dropdown.grid(row=0, column=n)
+
+	
+	def display(self):
+		self.frame.pack(pady=15)
+		
--- windows.py
+++ windows.py
@@ -1,36 +1,64 @@
 from tkinter import ttk
 import tkinter as tk
 import labels
+import widgets
 
 
 class LoginWindow:
 
-	def __init__(self, title="Login Prompt", dimensions="500x500"):
-		self.window = self.initialize_window(title, dimensions)
-		u, p = self.initialize_frames()
-		self.username_frame = u
-		self.password_frame = p
+	USERNAME_LABEL_MESSAGE = "Enter your username below."
+	PASSWORD_LABEL_MESSAGE = "Enter your password below."
+
+	USERNAME_LENGTH = 8
+	PASSWORD_LENGTH = 10
+
+	def __init__(self, title="Login Prompt", dimensions=None):
+		self.window = self.__initialize_window(title, dimensions)
+		self.username_frame, self.password_frame = self.__initialize_frames()
+		self.__initialize_labels()
+		self.__initialize_widgets()
 
 	
-	def initialize_window(self, title, dimensions):
+	def __initialize_window(self, title, dimensions):
 		window = tk.Tk()
 		window.title(title)
-		window.geometry(dimensions)
+
+		if dimensions:
+			window.geometry(dimensions)
+
 		return window
 
 
-	def initialize_frames(self):
+	def __initialize_frames(self):
 		
 		username_frame = tk.Frame(self.window, relief=tk.RAISED)
 		username_frame.pack()
 
 		separator = ttk.Separator(self.window, orient="horizontal")
-		separator.pack()
+		separator.pack(fill="x")
 
 		password_frame = tk.Frame(self.window, relief=tk.RAISED)
 		password_frame.pack()
 
 		return username_frame, password_frame
+
+	
+	def __initialize_labels(self):
+		
+		username_label = labels.PromptLabel(self.username_frame, self.USERNAME_LABEL_MESSAGE)
+		username_label.display()
+
+		password_label = labels.PromptLabel(self.password_frame, self.PASSWORD_LABEL_MESSAGE)
+		password_label.display()
+
+
+	def __initialize_widgets(self):
+
+		username_widget = widgets.DropdownTextEntry(self.username_frame, self.USERNAME_LENGTH)
+		username_widget.display()
+
+		password_widget = widgets.DropdownTextEntry(self.password_frame, self.PASSWORD_LENGTH)
+		password_widget.display()
 		
 	
 	def launch(self):
